61d5b19b48358a253bf6d73787ae77639ed70779
Thomas Sullivan <tdsullivan812@gmail.com>
2019-11-13 11:01:30 -0500

    First commit- very ratchet scene w/ 2d rigidbody

---
+++ Assets/ForceMove2D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceMove2D : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Vector2 vel;
+    Vector2 accel;
+    //Vector2 deltaX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            rigid.AddForce(Vector2.left * 0.2f);
+        }
+        if (Input.GetKey("d"))
+        {
+            rigid.AddForce(Vector2.right * 0.2f);
+        }
+        //if (Input.GetKey("w"))
+        //{
+          //  rigid.AddForceAtPosition(deltaZ, transform.position + Vector3.up * 0.2f);
+        //}
+        //if (Input.GetKey("s"))
+        //{
+           // rigid.AddForceAtPosition(-deltaZ, transform.position + Vector3.up * 0.2f);
+        //}
+    }
+}
