6a551c54065276b9a1b0889d0215209479d5df2e
Charlie Harper <charlottehdesigns@gmail.com>
2019-12-04 12:28:18 -0500

    *Fixed jelly sprite script again. -Changed the PC_Blob_1.prefab -added full loop -mainmenu -gameover -gamewin -credits -change scene script -

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public string NextScene;
+    public string GameOverScene;
+    public string GameWinScene;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextSceneSwitch () {
+        SceneManager.LoadScene(NextScene);
+    }
+
+    public void GameOver () {
+        SceneManager.LoadScene(GameOverScene);
+    }
+
+    public void GameWin (){
+        SceneManager.LoadScene(GameWinScene);
+    }
+}
