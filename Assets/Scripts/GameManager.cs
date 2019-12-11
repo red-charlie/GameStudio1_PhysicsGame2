2c3dcd384e3941a34abadddf673d0fa4fdf1ee51
Charlie Harper <charlottehdesigns@gmail.com>
2019-12-10 22:28:38 -0500

    Adding in some assets I forgot in the last push

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool created = false;
+     public GameObject GameManagerObject;
+
+   void Start(){
+if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/LoadZoneScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadZoneScript : MonoBehaviour
+{
+    
+    GameObject ChildZone;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+     Transform tf;
+     tf = transform.GetChild(0);
+    ChildZone = tf.gameObject;
+     //ChildZone = transform.GetChild(0);
+    // print("The current zone is" + ChildZone);
+    }
+
+
+    private void OnTriggerEnter2D (Collider2D collision){
+        //LoadZone();"
+       // print("Something has collided");
+        if(collision.gameObject.tag == "Slime"){
+        print("Loading this zone: " + ChildZone);
+        ChildZone.SetActive(true);
+       }
+    }
+
+    private void OnTriggerExit2D (Collider2D collision){
+       // CullZone();
+       if (collision.gameObject.name == "PC_Blob_1" 
+            && collision.gameObject.tag == "Slime" 
+            && ChildZone.activeSelf)
+        print("removing this zone! " + ChildZone);
+        ChildZone.SetActive(false);
+    }
+    
+
+}
